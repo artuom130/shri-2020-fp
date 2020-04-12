424540581261e1973027ee9a0411a07372245d89
Artem Loiko <artuom130@gmail.com>
2020-04-12 11:02:33 +0300

    Add sequence processing

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,73 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
-import Api from '../tools/api';
+import { pipe, tap, ifElse, andThen, length, otherwise, prop, has, always } from "ramda";
+import Api from "../tools/api";
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+const validator = (strNum) => {
+  const checkNumberSystem = (str) => /^\d+\.?\d+$/.test(str);
+  const numberLength = strNum.length;
+  return checkNumberSystem(strNum) && numberLength < 10 && numberLength > 2;
+};
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+const convertApiFetcher = api.get("https://api.tech/numbers/base");
+const convertApiRequest = (value) => convertApiFetcher({ from: 10, to: 2, number: value });
+const animalApiRequest = (id) => api.get(`https://animals.tech/${id}`, {});
+const getResult = prop("result");
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+const squareNum = (num) => Math.pow(num, 2);
+const reminderOfDivisionThree = (num) => num % 3;
+const convertToInt = (str) => Math.round(str);
+// eslint-disable-next-line no-unused-vars
+const clog = (m) => tap((v) => console.log(m, v));
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+const processSequence = (obj) => {
+  const { value, writeLog, handleSuccess, handleError } = obj;
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+  const logEffect = tap((val) => writeLog(val));
+  const errorEffect = tap(() => handleError("ValidationError"));
+  const networkErrorEffect = tap((v) => handleError(v));
+  const successEffect = tap((val) => handleSuccess(val));
+  const stop = always(undefined);
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+  const processConvertApiResult = pipe(
+    length,
+    logEffect,
+    squareNum,
+    logEffect,
+    reminderOfDivisionThree,
+    logEffect,
+    animalApiRequest,
+    otherwise(networkErrorEffect),
+    andThen(ifElse(has("result"), pipe(getResult, successEffect), stop))
+  );
+
+  const processConvertApiReq = pipe(
+    convertToInt,
+    logEffect,
+    convertApiRequest,
+    otherwise(networkErrorEffect),
+    andThen(ifElse(has("result"), pipe(getResult, logEffect, processConvertApiResult), stop))
+  );
+
+  const processValue = pipe(logEffect, ifElse(validator, processConvertApiReq, errorEffect));
+
+  processValue(value);
+};
 
 export default processSequence;
